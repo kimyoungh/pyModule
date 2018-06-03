944e0e7e4442daaa19fd33e5f7df3c9f399e5a66
kimyoungh <eluster107@gmail.com>
2018-06-03 17:22:31 +0900

    kimModule1

--- garchStockModule/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-import numpy as np
-
-setup(
-        ext_modules = cythonize("garchStockModule.pyx"),
-	include_dirs = [np.get_include()]
-)
